6ed1e1640a0d53234e832e6a780a553735df618b
martinsfgr <mauriciofagner2010@hotmail.com>
2025-01-14 14:21:44 -0300

    Ajustar função de soma para suportar todos os casos de sinais

--- bignumber.h
+++ bignumber.h
@@ -19,7 +19,7 @@ BigNumber* subtract_big_numbers(BigNumber *x, BigNumber *y);
 BigNumber* divide_big_numbers(BigNumber *x, BigNumber *y);
 BigNumber* multiply_big_numbers(BigNumber *x, BigNumber *y);
 
-int return_largest_big_number(BigNumber *x, BigNumber *y);
+int compare_big_numbers_modules(BigNumber *x, BigNumber *y);
 
 void print_big_number(BigNumber *x);
 void free_big_number(BigNumber *x);
--- client.c
+++ client.c
@@ -4,21 +4,21 @@
 
 int main() {
     BigNumber* x = create_big_number("-111");
-    BigNumber* y = create_big_number("11");
+    BigNumber* y = create_big_number("-11");
 
-    int comparison_big_numbers = return_largest_big_number(x, y);
+    int comparison_big_numbers = compare_big_numbers_modules(x, y);
     printf("A comparação entre os Big Numbers retornou: %d\n", comparison_big_numbers);
 
     BigNumber* sum = sum_big_numbers(x, y);
-    BigNumber* subtraction = subtract_big_numbers(x, y);
+    BigNumber* subtract = subtract_big_numbers(x, y);
 
     print_big_number(sum);
-    print_big_number(subtraction);
+    print_big_number(subtract);
 
     free_big_number(x);
     free_big_number(y);
     free_big_number(sum);
-    free_big_number(subtraction);
+    free_big_number(subtract);
 
     return 0;
 }
